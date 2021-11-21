f5ade3048964efbf999eb3471ab9657bb2dd6e6c
Timothy Lim Yong Lee <timothylimyonglee@gmail.com>
2021-11-21 23:38:44 +0800

    adding caution comment

--- cbam.py
+++ cbam.py
@@ -36,8 +36,7 @@ class SpatialAttention(nn.Module):
     def __init__(self, kernel_size=7):
         super().__init__()
 
-        self.spatial_conv = nn.Conv2d(2, 1, kernel_size, padding= (kernel_size-1) // 2)
-
+        self.spatial_conv = nn.Conv2d(2, 1, kernel_size, padding= (kernel_size-1) // 2) # Caution: Ensure same padding
 
     def forward(self, x):
         avg = torch.mean(x, dim=1, keepdim=True)
